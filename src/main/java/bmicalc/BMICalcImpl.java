67f1115cb6c3af23b791a2a4fa56eca85c1b8197
raulobreroberlanga <raulobreroberlanga@uma.es>
2023-02-07 19:22:10 +0100

    Implementación de los métodos de la calculadora

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -2,16 +2,35 @@ package bmicalc;
 
 public class BMICalcImpl implements BMICalc {
 
+	double bmi;
+	String categoria;
+
 	public double bmi(double mass, double height) {
-		return 0.0;
+		bmi= mass/(height*height);
+		return bmi;
 	}
 
 	public String category(double bmi) {
-		return "";
+		if(bmi<18.5) {
+			categoria="UNDERWEIGHT";
+		}else if (18.5<bmi && bmi<24.9){
+			categoria="NORMAL";
+		}else if(25.0<bmi && bmi<29.9) {
+			categoria="OVERWEIGHT";
+		}else {
+			categoria="OBESE";
+		}
+		return categoria;
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+		boolean t=false;
+		if(gender=='M' && waistCircumference>0.9) {
+			t=true;
+		}else if(gender=='F' && waistCircumference>0.8) {
+			t=true;
+		}
+		return t;
 	}
 
 }
