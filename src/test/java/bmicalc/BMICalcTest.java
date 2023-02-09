de9ee3ecf2ba33e4d5a021f408b1987b3bb7a92d
raulobreroberlanga <raulobreroberlanga@uma.es>
2023-02-09 18:25:14 +0100

    implementacion de las pruebas en Ecplise

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,6 +1,9 @@
 package bmicalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,73 @@ public class BMICalcTest {
 	@Test
 	@DisplayName("Example Test Method.")
 	public void bmi() {
-		assertEquals(true, true);
+		BMICalcImpl a=new BMICalcImpl();
+		assertEquals(a.bmi(1,1), 1);
+	}
+	
+	@Test
+	public void bmiPeso0() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertThrows(ArithmeticException.class,()->a.bmi(1,0));
+	}
+	@Test
+	public void masanegativa(){
+		BMICalcImpl a=new BMICalcImpl();
+		assertThrows(Exception.class,()->a.bmi(-1,1));
+	}
+	@Test
+	public void underweight() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertEquals(a.category(18.5),"UNDERWEIGHT");
+	}
+	@Test
+	public void Normal1() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertEquals(a.category(18.6),"NORMAL");
+	}
+	@Test
+	public void Normal2() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertEquals(a.category(29.9),"NORMAL");
+	}
+	@Test
+	public void Obese() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertEquals(a.category(20),"OBESE");
+	}
+	@Test
+	public void categoryBmiNegativo() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertThrows(Exception.class,()->a.category(-2));
+	}
+	@Test
+	public void aogenderMwc90(){
+		BMICalcImpl a=new BMICalcImpl();
+		assertTrue(a.abdominalObesity(0.9,'M'));
+	}
+	@Test
+	public void aogenderFwc80(){
+		BMICalcImpl a=new BMICalcImpl();
+		assertTrue(a.abdominalObesity(0.8,'F'));
+	}
+	@Test
+	public void aogenderMwc80(){
+		BMICalcImpl a=new BMICalcImpl();
+		assertFalse(a.abdominalObesity(0.8,'M'));
+	}
+	@Test
+	public void aogenderFwc70(){
+		BMICalcImpl a=new BMICalcImpl();
+		assertFalse(a.abdominalObesity(0.7,'F'));
+	}
+	@Test
+	public void genderDistinto() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertThrows(Exception.class,()->a.abdominalObesity(2,'D'));
+	}
+	@Test
+	public void waistCircumferenceNegativo() {
+		BMICalcImpl a=new BMICalcImpl();
+		assertThrows(Exception.class,()->a.abdominalObesity(-2, 'M'));
 	}
 }
