100395631054049aa8afe5ef40661f5fa878fd04
raulobreroberlanga <raulobreroberlanga@uma.es>
2023-04-25 13:31:45 +0200

    division de interfaz

--- src/main/java/bmicalc/BMICalc.java
+++ src/main/java/bmicalc/BMICalc.java
@@ -1,7 +1,7 @@
 package bmicalc;
 
 
-public interface BMICalc {
+public interface BMICalc extends MetabolicMetric {
 	
 	/**
 	 * Calculate the Body Mass Index (BMI) of a person.
@@ -24,15 +24,4 @@ public interface BMICalc {
 	 * @return	  String representing the BMI category.
 	 */
 	public String category(double bmi);
-	
-	/**
-	 * Determine whether a person has abdominal obesity.
-	 * A woman has abdominal obesity if her waist circumference is greater than 80 cm.
-	 * A man has abdominal obesity if his waist circumference is greater than 90 cm.
-	 * 
-	 * @param waistCircumference	Waist circumference.
-	 * @param gender				Male ('M') or Female ('F').
-	 * @return						True if the person has abdominal obesity, false otherwise.
-	 */
-	public boolean abdominalObesity(double waistCircumference, char gender);
 }
